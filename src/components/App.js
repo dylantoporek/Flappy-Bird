859f95c66fbface1bc7c6537a574a4774f716a87
Adam <inmon.adam@yahoo.com>
2022-07-10 12:44:04 -0500

    test commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ function App() {
   const screenWidth = window.innerWidth
   const screenHeight = window.innerHeight
   const birdLeft = screenWidth / 2
+  const [difficulty, setDifficulty] = useState("Easy")
   const [birdBottom, setBirdBottom]= useState(screenHeight / 2)
   const [obstaclesLeft, setObstaclesLeft]= useState(screenWidth)
   const [obstaclesLeftTwo, setObstaclesLeftTwo]= useState(screenWidth + screenWidth/2 + 30)
@@ -14,20 +15,23 @@ function App() {
   const [obstaclesNegHeightTwo, setObstaclesNegHeightTwo]= useState(0)
   const [isGameOver, setIsGameOver]= useState(false)
   const [score, setScore]= useState(0)
-  const gravity = 3
+  let gravity = 3;
+  const difCheck = (difficulty === "Easy" ? gravity : gravity = 6);
   let obstacleWidth = 60
   let obstacleHeight = 300
   let gap = 200
   let gameTimerId
   let obstaclesTimerId
   let obstaclesTimerIdTwo
+
+  // difficulty variety (changing speed/gravity)
   
 //start bird falling
 useEffect(() => {
   if (birdBottom > 0) {
     gameTimerId = setInterval(() => {
       setBirdBottom(birdBottom => birdBottom - gravity)
-    },30)
+    }, 30)
 
     return () => {
       clearInterval(gameTimerId)
@@ -37,12 +41,28 @@ useEffect(() => {
 }, [birdBottom])
 console.log(birdBottom)
   
+function handleFlap(){
+  console.log('flap')
+  // oppTimer = setInterval(() => {
+  //   setBirdBottom(birdBottom => birdBottom + 60)
+  // }) handle fluid animation
+  setBirdBottom(birdBottom + 60)
+}
 
-// document.onkeydown = handleFlap
+// spacebar keycode = 32
+
+document.body.onkeyup = (e) => {
+  if (e.key === " " ||
+      e.code ==="Space"          
+  ) {
+    handleFlap();
+  }
+}
 
   return (
     <div>
       <Bird birdBottom={birdBottom} birdLeft={birdLeft}/>
+      <Pipes />
     </div>
   );
 }
--- src/components/Pipes.js
+++ src/components/Pipes.js
@@ -3,7 +3,12 @@ import React from "react"
 function Pipes() {
   return (
     <div>
-      
+      <div className="pipe-container">
+        <div className="pipes">
+          <span className="pipe-top"></span>
+          <span className="pipe-bottom"></span>
+        </div>
+      </div>
     </div>
   );
 }
